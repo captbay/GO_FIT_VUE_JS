9294fa195bc38764b15ae8fe9c37b45b2b7276a7
captbay <agespramana9@gmail.com>
2023-04-30 05:44:57 +0700

    home

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-main>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-card>
+                        <v-card-title>
+                            <h1>Home</h1>
+                        </v-card-title>
+                        <v-card-text>
+                            <p>Ini adalah halaman home</p>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
+</template>
+<script>
+
+export default {
+
+};
+</script>
+
+  
